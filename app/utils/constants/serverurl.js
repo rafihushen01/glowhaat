31753d9f5d836e25830af4fc13d0e2853fc8b23d
rafihushen01 <movieanime641@gmail.com>
2026-04-08 01:48:46 +0600

    small fixes

--- app/utils/constants/serverurl.js
+++ app/utils/constants/serverurl.js
@@ -1,5 +1,5 @@
 
-export const serverurl1=process.env.NEXT_PUBLIC_SERVER_URL;
+export const serverurl=process.env.NEXT_PUBLIC_SERVER_URL;
 
-export const serverurl= "http://localhost:5000"
+export const serverurl1= "http://localhost:5000"
 export const frontendurl="https://khancosmetics.vercel.app"
